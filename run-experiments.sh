ef3bea7ee7b2a6954fdcff2931b46f2edc6b82fe
Antonio García-Domínguez <a.garcia-dominguez@aston.ac.uk>
2020-04-09 09:12:57 +0100

    Limit steps to 2000 for DQN

--- run-experiments.sh
+++ run-experiments.sh
@@ -14,23 +14,27 @@ SARSA_DIR="$BASEDIR/SARSA_changgang"
 EPISODES=100
 STEPS=20000
 
+# Fewer steps for DQN (20000 steps takes far too long)
+STEPS_DQN=2000
+
 # runExperiment(dbName, scriptDir)
 runExperiment() {
     DB_NAME="$1"
     SCRIPT_DIR="$2"
+    N_STEPS="$3"
 
     mongo "$DB_NAME" --eval "db.dropDatabase()" || true
     pushd "$SCRIPT_DIR"
-    python main.py --episode "$EPISODES" --step "$STEPS"
+    python main.py --database_name "$DB_NAME" --episode "$EPISODES" --step "$N_STEPS"
     popd
 }
 
 # Uncomment to run in sequence
-runExperiment "$DQN_DB" "$DQN_DIR" \
-    && runExperiment "$QL_DB" "$QL_DIR" \
-    && runExperiment "$SARSA_DB" "$SARSA_DIR"
+runExperiment "$DQN_DB" "$DQN_DIR" "$STEPS_DQN" \
+    & runExperiment "$QL_DB" "$QL_DIR" "$STEPS" \
+    & runExperiment "$SARSA_DB" "$SARSA_DIR" "$STEPS"
 
 # Uncomment to run in parallel
-# runExperiment "$DQN_DB" "$DQN_DIR" \
-#     & runExperiment "$QL_DB" "$QL_DIR" \
-#     & runExperiment "$SARSA_DB" "$SARSA_DIR"
+# runExperiment "$DQN_DB" "$DQN_DIR" "$STEPS_DQN" \
+#     & runExperiment "$QL_DB" "$QL_DIR" "$STEPS" \
+#     & runExperiment "$SARSA_DB" "$SARSA_DIR" "$STEPS"

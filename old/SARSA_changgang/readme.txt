950ee7773f3ea16721eeb0d84139cddb897428be
changgang <changgangzheng@qq.com>
2022-08-10 17:54:48 -0400

    Remove redundancy codes

--- old/SARSA_changgang/readme.txt
+++
@@ -1,7 +0,0 @@
-# To run this program, the following environment and packages should be downloaded.
-## environment： 
-python 3.6
-## Package: 
-numpy;
-math;
-torch;
--- old/restore-database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if test -z "$1"; then
-  echo "Usage: $0 archive-file.mongodump.xz"
-  exit 1
-fi
-ARCHIVE="$1"
-
-xzcat "$ARCHIVE" | mongorestore --archive
